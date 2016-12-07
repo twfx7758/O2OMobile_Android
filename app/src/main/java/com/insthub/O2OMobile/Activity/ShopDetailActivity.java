eafd6561838204b906ca8a9b5b7f4e7e4ae50ea6
twfx7758@163.com <twfx7758@163.com>
2016-12-07 16:44:38 +0800

    添加商铺详情页

--- app/src/main/java/com/insthub/O2OMobile/Activity/ShopDetailActivity.java
+++ app/src/main/java/com/insthub/O2OMobile/Activity/ShopDetailActivity.java
@@ -7,33 +7,39 @@ import com.BeeFramework.activity.BaseActivity;
 import com.BeeFramework.model.BusinessResponse;
 import com.external.androidquery.callback.AjaxStatus;
 import com.external.maxwin.view.IXListViewListener;
+import com.insthub.O2OMobile.Model.ShopDetailModel;
+import com.insthub.O2OMobile.Model.ShopInfoModel;
+import com.insthub.O2OMobile.Protocol.ApiInterface;
+import com.insthub.O2OMobile.Protocol.ShopDetailResponse;
 import com.insthub.O2OMobile.R;
+import com.insthub.O2OMobile.Utils.LocationManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ShopDetailActivity extends BaseActivity implements BusinessResponse, IXListViewListener {
+public class ShopDetailActivity extends BaseActivity implements BusinessResponse{
 
     public static final String SHOP_ID = "shop_id";
+    ShopDetailModel mDataModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_detail);
+
+        mDataModel = new ShopDetailModel(this);
+        mDataModel.addResponseListener(this);
+
+        LocationManager.getInstance().refreshLocation();
     }
 
     @Override
     public void OnMessageResponse(String url, JSONObject jo, AjaxStatus status) throws JSONException {
-
-    }
-
-    @Override
-    public void onRefresh(int id) {
-
-    }
-
-    @Override
-    public void onLoadMore(int id) {
-
+        if(url.endsWith(ApiInterface.SHOPINFO_DETAIL)){
+            if(null != jo){
+                ShopDetailResponse response = new ShopDetailResponse();
+                response.fromJson(jo);
+            }
+        }
     }
 }
